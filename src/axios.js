2d5de50ac488f5a6ce93b040d3a69148ea166b7c
HomeHusky <vitrannhat1@gmail.com>
2024-01-06 13:44:48 +0700

    edit axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://localhost:443/api/",
+  baseURL: "https://192.168.1.235:443/api/",
   withCredentials: true,
 });
